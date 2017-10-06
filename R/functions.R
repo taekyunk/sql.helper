70a9a536e597f76788d5bd2d2f582929f9f5eb13
Taekyun Kim <tkim@scholastic.com>
2017-10-06 10:10:48 -0400

    initial

---
+++ R/functions.R
@@ -0,0 +1,89 @@
+#' Concatenate all elements and surround the result by ()
+#'
+#' Concatenate the elements in x into a string and surround the result by parentheses.
+#' This does not add single quote to each element
+#'
+#' @param x vector
+#'
+#' @return a character string
+#'
+#' @examples
+#' paren:::add_paren(1:5)
+add_paren <- function(x){
+    middle <- paste(x, collapse = ",")
+    paste0("(", middle, ")")
+}
+
+
+#' Put each element in single quotes
+#'
+#' @param x vector
+#'
+#' @return a character string
+#'
+#' @examples
+#' paren:::single_quote_each(letters[1:5])
+single_quote_each <- function(x){
+    paste0("'", x, "'")
+}
+
+
+#' Surround the elements in parenthesis with optional single quotes
+#'
+#' Mainly to use with SQL IN operator.
+#' Surround the elements in parenthesis.
+#' Each element will be single quoted depending on the data type.
+#'
+#' @param x vector
+#'
+#' @return a character string
+#' @export
+#'
+#' @examples
+#' paren(1:5)
+#' paren(letters[1:5])
+#' # Use as follows to build SQL queries
+#' paste("select * from schema.table where id in", paren(1:5), ";")
+#' paste("select * from schema.table where id in", paren(letters[1:6]), ";")
+paren <- function(x){
+    if(is.numeric(x)){
+        add_paren(x)
+    }  else {
+        add_paren(single_quote_each(x))
+    }
+}
+
+
+#' Concatenate by comma
+#'
+#' @param vars character vector
+#'
+#' @return string with comma separated
+#'
+#' @examples
+#' comma_concat(c("x", "y"))
+comma_concat <- function(vars){
+    paste(vars, collapse = ", ")
+}
+
+#' Count the cases for the combination of variables from the table
+#'
+#' @param vars character vector of variable names
+#' @param table_name string of table name
+#'
+#' @return sql query string
+#' @export
+#'
+#' @examples
+#' library(tidyverse)
+#' c("x", "y") %>%
+#' count_from("table_name")
+count_from <- function(vars, table_name){
+    vars_cat <- comma_concat(vars)
+    groups_cat <- comma_concat(seq_along(vars))
+    query <- paste(
+        "select", vars_cat, ", count(*) as n",
+        "from", table_name,
+        "group by", groups_cat, ";")
+    query
+}
---
+++ README.md
@@ -0,0 +1,52 @@
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+sql.helper
+==========
+
+The goal of sql.helper is to help building SQL queries in R
+
+Install
+-------
+
+    # install.packages('devtools')
+    devtools::install_github("taekyunk/sql.helper")
+
+Example: `paren()`
+------------------
+
+This is a basic example of using `paren()` with SQL IN operator.
+
+``` r
+id_char <- as.character(1:5)
+id_num <- 3:8
+
+# paren() will add single quotes if the field is not numeric
+paren(id_char)
+#> [1] "('1','2','3','4','5')"
+paren(id_num)
+#> [1] "(3,4,5,6,7,8)"
+
+# to match character with numeric
+paren(as.numeric(id_char))
+#> [1] "(1,2,3,4,5)"
+
+# to match numeric with character
+paren(as.character(id_num))
+#> [1] "('3','4','5','6','7','8')"
+
+# typical use case will be something like this
+query <- paste("select * from tablename where id in", paren(id_num), ";")
+print(query)
+#> [1] "select * from tablename where id in (3,4,5,6,7,8) ;"
+```
+
+Example: `count_from()`
+-----------------------
+
+`count_from()` can be used to build SQL query to count the combination of variables.
+
+``` r
+library(tidyverse)
+c("x", "y") %>% 
+    count_from("schema.table")
+#> [1] "select x, y , count(*) as n from schema.table group by 1, 2 ;"
+```
